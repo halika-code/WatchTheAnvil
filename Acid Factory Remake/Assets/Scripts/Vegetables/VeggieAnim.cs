5ff244f229d4d4fd9d09eab9201f16e8291deb25
gak8 <112573854+halika-code@users.noreply.github.com>
2023-10-07 15:04:00 +0100

    Worked on the vegetables wiggling. defining a Vector3 with 2 parameters is a bad idea.

--- Acid Factory Remake/Assets/Scripts/Vegetables/VeggieAnim.cs
+++ Acid Factory Remake/Assets/Scripts/Vegetables/VeggieAnim.cs
@@ -1,19 +1,20 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
-public static class VeggieAnim{
+public static class VeggieAnim {
     /**
      * <summary>The idea here is to have the carrot pop out of the ground then abruptly stop
      * <para>The carrot is supposed to shoot up then slow slightly (and naturally) before abruptly stopping, all popped out or hidden</para></summary>
      */
     public static IEnumerator animateCarrot(Rigidbody targetCBody, VegetableVisibility.VegState state) {
-        var y = 10f;
+        var y = 5f;
         var cBodyP = targetCBody.position;
-        if (state is VegetableVisibility.VegState.Hidden) {
+        if (state is VegetableVisibility.VegState.Visible) {
             y *= -1;
-        } while (y > 1) { //todo check if I have the right idea, then fine-tune this condition to terminate when the y will not be too slow
-            y = Mathf.Lerp(cBodyP.y, cBodyP.y + y, 0.5f);
-            targetCBody.position = new Vector3(cBodyP.x, y);
+        } while (Math.Abs(y) > 1) {
+            y = Mathf.Lerp(cBodyP.y, cBodyP.y + y, 0.2f); //todo test this out, the veg jumps out THEN ducks down ... perhaps check the state if the veg is trying to wiggle between states
+            targetCBody.position = new Vector3(cBodyP.x, y, cBodyP.z);
             yield return new WaitForSeconds(0.5f);
         } 
     }
