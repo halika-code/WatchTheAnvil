04605629e4a1d734647a9d0b69c6a58003125791
gak8 <112573854+halika-code@users.noreply.github.com>
2024-01-02 21:05:31 +0100

    Tinkered with the camera a bit, rewrote some unnecessary math and started working on the only bug in the camera system

--- Acid Factory Remake/Assets/Scripts/Camera/Cam.cs
+++ Acid Factory Remake/Assets/Scripts/Camera/Cam.cs
@@ -1,14 +1,15 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using static Character_Controller;
 public class Cam : MonoBehaviour {
-    
-    private static Camera cam;
-    private static readonly Vector3 NormalDistance = new (-1.23f, 50f, -120f); //this is the distance relative to the player where the camera has the player in the center
+    private Camera cam;
+    private readonly Vector3 normalDistance = new (-1.23f, 50f, -120f); //this is the distance relative to the player where the camera has the player in the center
 
     private void Start() {
         if (cam != null) {
-            cam.transform.position = NormalDistance;
+            cam.transform.position = normalDistance;
         } else {
             cam = Camera.main;
         }
@@ -21,7 +22,7 @@ public class Cam : MonoBehaviour {
     /**
      * <summary>Moves the cam if it is deemed to have left a border</summary>
      */
-    private static void moveCam() {
+    private void moveCam() {
         if (checkIfLeftBorder()) { //if the player is stationary or inside the border
             moveFollowPlayer();
         } 
@@ -31,16 +32,12 @@ public class Cam : MonoBehaviour {
      * <summary>Follows quickly the player using smooth scrolling</summary>
      * <remarks><see cref="Mathf.Lerp(float, float, float)"/> is used here to scroll smoothly</remarks>
      */
-    private static void moveFollowPlayer() {
+    private void moveFollowPlayer() {
         var pBody = getPlayerBody().position;
         var camPos = cam.transform.position;
-        var leftBorderAt = haveLeftBorder(camPos, pBody);
-        if (leftBorderAt[0]) {
-            camPos.x = Mathf.Lerp(camPos.x, NormalDistance.x + pBody.x, 0.02f);
-        } if (leftBorderAt[1]) {
-            camPos.y = Mathf.Lerp(camPos.y, NormalDistance.y + pBody.y, 0.02f);
-        } if (leftBorderAt[2]) {
-            camPos.z = Mathf.Lerp(camPos.z, NormalDistance.z + pBody.z, 0.02f);
+        var asd = haveLeftBorder(camPos, pBody);
+        foreach (var i in asd) {
+            camPos[i] = Mathf.Lerp(camPos[i], normalDistance[i] + pBody[i], 0.02f);
         } cam.transform.position = camPos;
     }
 
@@ -50,14 +47,20 @@ public class Cam : MonoBehaviour {
      * <param name="pBody">The player's current position</param>
      * <returns>A list of booleans corresponding to each cardinal sides that the player have left the border at</returns>
      */
-    public static bool[] haveLeftBorder(Vector3 camPos, Vector3 pBody) {
-        var camPosPar = Math.Sign(camPos.x) is 1; //cameraPositionParity
-        var pBodyPar = Math.Sign(pBody.x) is 1; //playerBodyParity
-        if (camPosPar && !pBodyPar || !camPosPar && pBodyPar) { //checking here to not have a case of (45[camPos] + (-5)[pBody])
-            camPos.x *= Math.Sign(camPos.x); //doesn't matter which variable I multiply, since multiplying by 1 doesn't ruin anything
-            pBody.x *= Math.Sign(pBody.x); 
-        } return new[] { Math.Abs(camPos.x - pBody.x) > NormalDistance.x + 2f, 
-                Math.Abs(camPos.y - pBody.y) > NormalDistance.y + 2f, Math.Abs(camPos.z - pBody.z) > NormalDistance.z + 2f };
+    private List<int> haveLeftBorder(Vector3 camPos, Vector3 pBody) {
+        var asd = Math.Abs(camPos.x - pBody.x) > normalDistance.x + 2f; //todo check the z, since the cam doesn't want to follow the player upwards
+        var asd2 = Math.Abs(camPos.y - pBody.y) > normalDistance.y + 2f;
+        var asd3 = Math.Abs(camPos.z - pBody.z) > normalDistance.z + 2f;
+        var leftBorderAt = new[] {asd,asd2
+           , asd3 };
+        if (!leftBorderAt.Contains(true)) {
+            return new List<int> {-1};
+        } var ret = new List<int>();
+        for (var i = 0; i < 3; i++) {
+            if (leftBorderAt[i]) {
+                ret.Add(i);
+            }
+        } return ret;
     }
 
     /**
@@ -65,11 +68,7 @@ public class Cam : MonoBehaviour {
      * <returns>true if the player is deemed to have passed the border at any cardinal directions, false otherwise</returns>
      * <remarks>This function could be integrated (I imagine) with a LINQ style call into the <see cref="moveCam"/> function</remarks>
      */
-    private static bool checkIfLeftBorder() {
-        foreach (var borderSide in haveLeftBorder(cam.transform.position, getPlayerBody().position)) {
-            if (borderSide) {
-                return true;
-            }
-        } return false;
+    private bool checkIfLeftBorder() {
+        return !haveLeftBorder(cam.transform.position, getPlayerBody().position).Contains(-1);
     }
 }
