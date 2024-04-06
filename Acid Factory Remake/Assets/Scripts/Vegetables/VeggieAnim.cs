fd1915d35696ff21bc20b949d873f4bdad565500
gak8 <112573854+halika-code@users.noreply.github.com>
2024-04-06 17:34:26 +0100

    Made the animations popping up to a perfect amount

--- Acid Factory Remake/Assets/Scripts/Vegetables/VeggieAnim.cs
+++ Acid Factory Remake/Assets/Scripts/Vegetables/VeggieAnim.cs
@@ -3,25 +3,21 @@ using UnityEngine;
 
 public static class VeggieAnim {
 
-    private static readonly float[] RaiseHeights = {2.58f, 1.33f, 0.52f, 3.2f}; //big, medium, small veggie and flower
+    private static readonly float[] RaiseHeights = {2.58f*2, 1.33f*2, 0.52f*2, 3.2f}; //big, medium, small veggie and flower
     /**
      * <summary>The idea here is to have the carrot pop out of the ground then abruptly stop
      * <para>The carrot is supposed to shoot up then slow slightly (and naturally) before abruptly stopping, all popped out or hidden</para></summary>
      */
     public static IEnumerator animateCarrot(Rigidbody targetCBody, VegetableVisibility.VegState state) {
-        var asd2 = Character_Controller.getParentName(targetCBody.transform);
-        var asd = getModifier(targetCBody, state);
         var y = targetCBody.position.y; //original y speed
-        for (var i = 8; i > 0; i--) {
-            targetCBody.transform.position = new Vector3(targetCBody.position.x, y + getModifier(targetCBody, state), targetCBody.position.z); 
-            yield return new WaitForSeconds(0.008f); //todo the modifier still doesn't work, the position is too small for some reason
+        for (var i = 6; i > 0; i--) { //todo wanna make it smooth
+            targetCBody.position = new Vector3(targetCBody.position.x, y + (getModifier(targetCBody, state) / i), targetCBody.position.z);
+            yield return new WaitForSeconds(0.008f);
         } 
     }
 
     /**
-     * <summary> todo the idea here is to have the modifier above be set to the corresponding value
-     * (which is the amount the vegetable (given it's size) should move up/down)
-     * todo also add a case for flower (see how that can be done)
+     * <summary>
      * </summary>
      */
     private static float getModifier(Component targetBody, VegetableVisibility.VegState state) {
